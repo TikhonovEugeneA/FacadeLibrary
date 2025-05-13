3bdeaf6ef1115cd363c02f12dcc36a672e6d4025
TikhonovEugeneA <tih050504@gmail.com>
2025-05-14 03:23:24 +0700

    Создал 3 фасадных интерфейса: ICustomerFacade, ISellerFacade, IWarehouserFacade. Добавил в зависимости проекта пакет Nqgsql

---
+++ FacadeLibrary/DBFacade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace FacadeLibrary
+{
+    public class DBFacade
+    {
+    }
+}
